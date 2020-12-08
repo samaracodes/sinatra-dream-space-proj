addc90c1141863cb3f1c7123a84ff5698b94a855
samara-amanda <samara.amanda01@gmail.com>
2020-12-08 04:11:13 -0500

    enable sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "our_awesome_dream_space_app"
+    #if someone doesnt have the sesssion scret and tries 
+    # to imitate you they wont be able to.
   end
 
   get "/" do
