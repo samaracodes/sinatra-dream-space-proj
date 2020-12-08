5ae36c41a48b890dac57674f38ebdb9f261f8e78
samara-amanda <samara.amanda01@gmail.com>
2020-12-08 11:54:35 -0500

    removed comments

--- db/migrate/20201208085412_create_users_table.rb
+++ db/migrate/20201208085412_create_users_table.rb
@@ -4,8 +4,7 @@ class CreateUsersTable < ActiveRecord::Migration
       t.string :name
       t.string :email
       t.string :password_digest 
-      #We're using password_digest because we're 
-      # utilizing bcrypt to encrypt the passwords
+      
       t.timestamps null: false
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,10 @@
-#here is where I will create some seed data to work with and test associations
 
-#Create 2 users
 samara = User.create(name: "Samara", email: "sam@example.com", password: "password")
 cameron = User.create(name: "Cameron", email: "cameron@example.com", password: "password")
 
-# Create some dream posts
+
 
 DreamPost.create(content: "Today is Tuesday and it's great", user_id: samara.id)
 
-# Use ActiveRecord to pre-associate data:
 samara.dream_posts.create(content: "We are building a SUPER FUN Sinatra App! YAY")
 cameron.dream_posts.create(content: "Its 2AM and Im writing code!!!")
