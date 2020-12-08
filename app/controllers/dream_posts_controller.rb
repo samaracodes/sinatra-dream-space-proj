fe2c2716ce2a815bb8a383bf53373e2d7e89766b
samara-amanda <samara.amanda01@gmail.com>
2020-12-08 05:23:35 -0500

    add post dream_posts route

--- app/controllers/dream_posts_controller.rb
+++ app/controllers/dream_posts_controller.rb
@@ -6,7 +6,21 @@ class DreamPostsController < ApplicationController
     end
 
     # post dream_posts to create a new dream post
-
+    post "/dream_posts" do
+        # we want to create a new dream post and save it to the DB. 
+        # I also only want to create a dream post if a user is logged in. 
+        if !logged_in?
+            redirect '/'
+        end
+        # I only want to save the post if it has some content. 
+        if params[:content] != ""
+            #create a new post
+            @dream_post = DreamPost.create(content: params[:content], user_id: current_user.id)
+            redirect "/dream_posts/#{@dream_post.id}"
+        else
+            redirect '/dream_posts/new'
+        end
+    end
     # show route for a dream posts
 
     # index route for all dream posts
