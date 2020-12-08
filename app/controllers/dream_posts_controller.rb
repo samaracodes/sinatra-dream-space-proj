f86976b1c74ad81cb57e9d8f5d1d5c8eecd93c5c
samara-amanda <samara.amanda01@gmail.com>
2020-12-08 05:29:22 -0500

    added show route for posts

--- app/controllers/dream_posts_controller.rb
+++ app/controllers/dream_posts_controller.rb
@@ -1,9 +1,13 @@
 class DreamPostsController < ApplicationController
+    
+    
     # get dream_posts/new to render a form to create a new 
     # entry
     get '/dream_posts/new' do
        erb :'/dream_posts/new'
     end
+
+
 
     # post dream_posts to create a new dream post
     post "/dream_posts" do
@@ -21,7 +25,21 @@ class DreamPostsController < ApplicationController
             redirect '/dream_posts/new'
         end
     end
+
+
+
     # show route for a dream posts
+    get '/dream_posts/:id' do
+        @dream_post = DreamPost.find(params[:id])
+        erb :'/dream_posts/show'
+    end
+
+    get '/dream_posts/:id/edit' do
+        #this route should send us to dream_posts/edit.erb
+        # which will render an edit form
+        erb :'/dream_posts/edit'
+    end
+
 
     # index route for all dream posts
 end
