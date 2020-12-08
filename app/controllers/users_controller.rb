84d647574ff206a5e1d26f5f159f3a1f0db1be89
samara-amanda <samara.amanda01@gmail.com>
2020-12-08 06:07:35 -0500

    fixed bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,30 +7,31 @@ class UsersController < ApplicationController
     end
 
 
-    #this purpose of this route is to receive the login form
-    # find the user and log the user in (creste a session)
+        #this purpose of this route is to receive the login form
+        # find the user and log the user in (creste a session)
     post '/login' do
             # params looks like: {email: "user@user.com", password: "password""}
             # Find the user
         @user = User.find_by(email: params[:email])
             #Authenticaste the user - verify the user is who they say they are
             # they have the credentials - email/password combo
-        if @user.authenticate(params[:password])
+        if @user && @user.authenticate(params[:password])
              # log the user in - create the user session
             session[:user_id] = @user.id #this is what is actually logging the user in
                 # redirect to the user's landing page(show page)
             puts session
             redirect "/users/#{@user.id}"
         else
-            # tell the user they entered invalid credentials
-            # redirect them to the login page
-
+                # tell the user they entered invalid credentials
+                # redirect them to the login page
+            flash[:message] = "The email and/or password entered was not a match. Please try again."
+            redirect '/login'
         end
     end
 
         # this route's job is to render the signup form
     get '/signup' do
-        #erb (render) a view
+            #erb (render) a view
         erb :signup
     end
 
@@ -51,15 +52,15 @@ class UsersController < ApplicationController
             # where do I go now?
             redirect to "/users/#{@user.id}"
         else
-            # not valid input
+                # not valid input
             redirect to '/signup'
-        #it would be ideal to display a message
+                #it would be ideal to display a message
         end
     end
 
-    #user SHOW route
+        #user SHOW route
     get '/users/:id' do
-        #what do I need to do first?
+            #what do I need to do first?
         @user = User.find_by(id: params[:id])
     
         erb :'/users/show'
