763f6788601c6a0a310faf8d6b0cadeabada70be
samara-amanda <samara.amanda01@gmail.com>
2020-12-08 05:19:11 -0500

    add dream posts controller

---
+++ app/controllers/dream_posts_controller.rb
@@ -0,0 +1,13 @@
+class DreamPostsController < ApplicationController
+    # get dream_posts/new to render a form to create a new 
+    # entry
+    get '/dream_posts/new' do
+       erb :'/dream_posts/new'
+    end
+
+    # post dream_posts to create a new dream post
+
+    # show route for a dream posts
+
+    # index route for all dream posts
+end
