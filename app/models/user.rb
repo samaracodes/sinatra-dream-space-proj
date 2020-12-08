c1bc6c2e226dc027bc7ef7be4f35b7da4256fee8
samara-amanda <samara.amanda01@gmail.com>
2020-12-08 06:36:06 -0500

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
     has_secure_password
 
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    #making sure these prevent saving or creating anything in the database
+    #if the conditions are not met. 
+
+
     has_many :dream_posts
 end
