0b5d6e0512932db051500c899a727a18cfc522d1
Sam <samara.amanda01@gmail.com>
2022-04-09 08:44:35 -0400

    deploy attempt

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
-  :database_file, "./database.yml"
+  :database_file => "./database.yml"
 )
 
 require './app/controllers/application_controller'
